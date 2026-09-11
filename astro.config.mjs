afd1472c638d1d943a68948bb533e8188953043c
Ronny Shani <63248335+ironnysh@users.noreply.github.com>
2026-09-11 19:08:02 +0000

    Updated the Storyblok integration import to the named storyblok export

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import storyblok from "@storyblok/astro";
+import { storyblok } from "@storyblok/astro";
 import { loadEnv } from "vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
